6f8b40e59856b0d4ed591d03ce69d62c32f56b1c
Artur Petryk <petrikartur@gmail.com>
2021-09-16 20:59:20 +0300

    logging settings

--- fakeCSV/settings.py
+++ fakeCSV/settings.py
@@ -157,3 +157,19 @@ DATABASES['default'].update(db_from_env)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
